c5a074933059bd1583542aadfea7a81c3d607ea5
OmPrakash05 <omprakashkhandale5@gmail.com>
2026-03-12 19:59:21 +0530

    Feature: Create contact page

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,10 @@
+const Contact = () => {
+    return ( 
+        <div>
+            <h1>Contact Us</h1>
+            <p>Feel free to reach out for recipe suggestions or feedback.</p>
+        </div>
+     );
+}
+ 
+export default Contact;
