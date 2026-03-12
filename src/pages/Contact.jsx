bd6a423bdb2055cdced8a8e8bcac8d673eb66d33
OmPrakash05 <omprakashkhandale5@gmail.com>
2026-03-12 23:40:22 +0530

    Feature: Create contact page with tailwind and add responsive form layout

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,41 @@
 const Contact = () => {
     return ( 
-        <div className="max-w-4xl mx-auto py-20 text-center">
-            <h1 className="text-4xl font-bold mb-4">Contact Us</h1>
-            <p className="text-grey-600 text-lg">Feel free to reach out for recipe suggestions or feedback.</p>
-        </div>
+        <section className="max-w-4xl mx-auto py-16 px-6 ">
+            <h1 className="text-4xl font-bold  text-center mb-4">Contact Us</h1>
+            <p className="text-grey-600 text-center mb-10 text-lg">Feel free to reach out for recipe suggestions or feedback.</p>
+            <div className="bg-white shadow-md rounded-xl p-8">
+                <form className="space-y-6">
+                    <div>
+                        <label className="block text-gray-700 mb-2">Name</label>
+                        <input
+                        type="text"
+                        placeholder="Enter your name"
+                        className="w-full border rounded-lg px-4 py-2 outline-none focus:ring-2 focus:ring-orange-400"
+                        
+                        />
+                    </div>
+                    <div>
+                        <label className="block text-gray-700 mb-2">Email</label>
+                        <input
+                        type="email"
+                        placeholder="Enter your email"
+                        className="w-full border rounded-lg px-4 py-2 outline-none focus:ring-2 focus:ring-orange-400"/>
+                    </div>
+                    <div>
+                        <label className="block text-gray-700 mb-2">Message</label>
+                        <textarea rows="4"
+                        placeholder="Write your message"
+                        className="w-full border rounded-lg px-4 py-2 outline-none focus:ring-2 focus:ring-orange-400"
+                        ></textarea>
+                    </div>
+
+                    <button
+                    type="submit"
+                    className="w-full bg-orange-500 text-white py-3 rounded-lg hover:bg-orange-600 transition"
+                    >Send Message</button>
+                </form>
+            </div>
+        </section>
      );
 }
  
