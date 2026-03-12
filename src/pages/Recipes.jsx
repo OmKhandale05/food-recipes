015e3d2f898394a6b8704a73f9c3b4d94c2c410c
OmPrakash05 <omprakashkhandale5@gmail.com>
2026-03-12 19:54:17 +0530

    Feature: Add react router and create recipes page

---
+++ src/pages/Recipes.jsx
@@ -0,0 +1,10 @@
+const Recipes = () => {
+    return ( 
+        <div>
+            <h1>All Recipes</h1>
+            <p>Browse our complete collection of delicious recipes.</p>
+        </div>
+     );
+}
+ 
+export default Recipes;
